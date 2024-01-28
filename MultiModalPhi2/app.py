cec9b803e32d37c3af90f41fa120d6db96c8e271
Guna Satya Sri Koppula <61241928+GunaKoppula@users.noreply.github.com>
2024-01-28 22:30:34 +0530

    Update app.py

--- MultiModalPhi2/app.py
+++ MultiModalPhi2/app.py
@@ -2,10 +2,12 @@ import gradio as gr
 from PIL import Image
 from inference.main import MultiModalPhi2
 
+# gr.themes.builder()
+
 messages = []
 
 multimodal_phi2 = MultiModalPhi2(
-    modelname_or_path="RaviNaik/Llava-Phi2",
+    modelname_or_path="GunaKoppula/Llava-Phi2",
     temperature=0.2,
     max_new_tokens=1024,
     device="cpu",
@@ -55,60 +57,53 @@ def run(history, text, image, audio_upload, audio_mic):
     if image is not None:
         image = Image.open(image)
     outputs = multimodal_phi2(text, audio, image)
-    # outputs = ""
-
+   
     history.append((None, outputs.title()))
     return history, None, None, None, None
 
 
-with gr.Blocks() as demo:
-    gr.Markdown("## MulitModal Phi2 Model Pretraining and Finetuning from Scratch")
-    gr.Markdown(
-        """This is a multimodal implementation of [Phi2](https://huggingface.co/microsoft/phi-2) model.
-        
-        Please find the source code and training details [here](https://github.com/RaviNaik/ERA-CAPSTONE/MultiModalPhi2).
-        
-        ### Details:
-        1. LLM Backbone: [Phi2](https://huggingface.co/microsoft/phi-2)
-        2. Vision Tower: [clip-vit-large-patch14-336](https://huggingface.co/openai/clip-vit-large-patch14-336)
-        3. Audio Model: [Whisper Tiny](https://huggingface.co/openai/whisper-tiny) 
-        4. Pretraining Dataset: [LAION-CC-SBU dataset with BLIP captions(200k samples)](https://huggingface.co/datasets/liuhaotian/LLaVA-Pretrain)
-        5. Finetuning Dataset: [Instruct 150k dataset based on COCO](https://huggingface.co/datasets/liuhaotian/LLaVA-Instruct-150K)
-        6. Finetuned Model: [RaviNaik/Llava-Phi2](https://huggingface.co/RaviNaik/Llava-Phi2)
-        """
-    )
+
+with gr.Blocks(theme='upsatwal/mlsc_tiet') as demo:
+
+    with gr.Column():
+        gr.Markdown("## MulitModal Phi2 Model Pretraining and Finetuning from Scratch")
+        gr.Markdown("This is a multimodal implementation of [Phi2](https://huggingface.co/microsoft/phi-2) model. Please find the source code and training details [here](https://github.com/GunaKoppula/ERAV1-CAPSTONE).")
+
+
     with gr.Row():
+        chatbot = gr.Chatbot(
+            avatar_images=("🧑", "🤖"),
+            height=550,
+        )
+                
+    with gr.Row():       
         with gr.Column(scale=4):
             # Creating a column with a scale of 6
             with gr.Box():
-                with gr.Row():
-                    # Adding a Textbox with a placeholder "write prompt"
-                    prompt = gr.Textbox(
-                        placeholder="Enter Prompt", lines=2, label="Query", value=None
-                    )
-                # Creating a column with a scale of 2
-                with gr.Row():
-                    # Adding image
-                    image = gr.Image(type="filepath", value=None)
-                # Creating a column with a scale of 2
-                with gr.Row():
-                    # Add audio
-                    audio_upload = gr.Audio(source="upload", type="filepath")
-                    audio_mic = gr.Audio(
-                        source="microphone", type="filepath", format="mp3"
-                    )
-
-        with gr.Column(scale=8):
-            with gr.Box():
-                with gr.Row():
-                    chatbot = gr.Chatbot(
-                        avatar_images=("🧑", "🤖"),
-                        height=550,
-                    )
-                with gr.Row():
+                
+                with gr.Column():
+                    
+                    with gr.Row():
+                        # Adding a Textbox with a placeholder "write prompt"
+                        prompt = gr.Textbox(
+                            placeholder="Enter Prompt", lines=2, label="Query", value=None
+                        )
+                    # Creating a column with a scale of 2
+    
+                    with gr.Row():
+                        # Adding image
+                        image = gr.Image(type="filepath", value=None)
+                    # Creating a column with a scale of 2
+                    with gr.Row():
+                        # Add audio
+                        audio_upload = gr.Audio(source="upload", type="filepath")
+                        audio_mic = gr.Microphone(source="microphone", type="filepath", format="mp3")
+                        
                     # Adding a Button
-                    submit = gr.Button()
-                    clear = gr.Button(value="Clear")
+                    with gr.Row():
+                        submit = gr.Button()
+                    with gr.Row():
+                        clear = gr.Button(value="Clear")
 
     submit.click(
         add_content,
@@ -125,4 +120,4 @@ with gr.Blocks() as demo:
         outputs=[prompt, image, audio_upload, audio_mic, chatbot],
     )
 
-demo.launch(server_port=8881)
+demo.launch()
